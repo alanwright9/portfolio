24afa3687c4074d2a99630866eed3d2d7c68ad65
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-14 22:18:32 -0400

    seeing if anything needs updating

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
-# build
+
+# build project first
 npm run build
-# navigate into the build output directory
+
+# enter build directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
+# commit changes in this directory to the subtree
 git init
+git branch -M main
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:techtress/vue-portfolio.git main:gh-pages
+git push -f https://github.com/techtress/vue-portfolio.git main:gh-pages
 cd -
