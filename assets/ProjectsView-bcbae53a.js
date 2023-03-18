78b337e03a7a0c88ab88712de6016fa2c80a80c4
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-17 22:37:42 -0400

    deploy

---
+++ assets/ProjectsView-bcbae53a.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-6f692b66.js";const r={};function t(n,s){return c(),o("h1",null,"Projects")}const a=e(r,[["render",t]]);export{a as default};
--- assets/ResumeView-048aaf0a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as s,c,a as o}from"./index-c79cd813.js";const t={},_={class:"main_view"},n=o("h1",null,"Resume",-1),a=[n];function r(i,d){return s(),c("div",_,a)}const m=e(t,[["render",r]]);export{m as default};
