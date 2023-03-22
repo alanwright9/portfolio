9f13cb12d58c67f4de4d8e200a7aa1b02cc98aa7
Techtress <47042355+Techtress@users.noreply.github.com>
2023-03-22 05:26:38 -0400

    added temp about/contact pages

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="main_left">
-    <h1>About</h1>
+  <div>
+    <div class="text-start">
+      <h1 class="mb-3 mb-md-4">About</h1>
+      <p class="fs-5">
+        This page is currently under construction. For now, check out my
+        <a class="text-decoration-underline"
+           href="https://docs.google.com/document/d/1QCYzvTPekeSJjLqOrzjsPcR0c3Xj0MgnykM9mPjIv6M/edit?usp=sharing"
+           target="_blank">
+          Web Developer Resume
+        </a>
+      </p>
+    </div>
   </div>
 </template>
--- src/views/ContactMeView.vue
+++ src/views/ContactMeView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="main_left">
-    <h1>Contact Me</h1>
+  <div>
+    <div class="text-start">
+      <h1 class="mb-3 mb-md-4">Contact Info</h1>
+      <p class="fs-5">
+        This page is currently under construciton and will feature a full contact form.
+        For now, here is my business email address.
+      </p>
+      <hr class="m-4 m-md-5">
+      <p class="fs-5">Contact me at: techtress (dot) alan (at) gmail (dot) com</p>
+    </div>
   </div>
 </template>
